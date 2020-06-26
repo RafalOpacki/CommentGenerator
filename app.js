e325b0a04bf07c0771d3a61200cd6055ad98e911
Rafal Opacki <rafal.opacki@jit.team>
2020-06-27 00:34:04 +0200

    Connect DB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+MongoClient.connect(
+  process.env.CONNECTION_STRING,
+  { useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB');
+  },
+);
 
 app.listen(port, () => console.log(`Server is live on port ${port}`));
 
